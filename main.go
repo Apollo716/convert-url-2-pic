cbc5abdbdbbc82e09cc302d76801213f56924e32
Apollo716 <0014maato@gmail.com>
2022-04-27 20:19:42 +0900

    wip

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/jszwec/csvutil"
 )
 
 type Url struct {
-	url string `csv:"url"`
+	url string `csv:"URL"`
 }
+
+const (
+	dirPath = "./src"
+)
 
 func main() {
 	var urls []*Url
 	byte, err := ioutil.ReadFile("url.csv")
+	log.Println(byte)
 	if err != nil {
 		log.Printf("readFile err: err is %s", err.Error())
 		return
@@ -21,5 +30,23 @@ func main() {
 	if err = csvutil.Unmarshal(byte, &urls); err != nil {
 		log.Printf("csvUnmarshal err: err is %s", err.Error())
 		return
+	}
+	log.Println(urls[0])
+	for i, urlField := range urls {
+		var url string = urlField.url
+		log.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("fail to get %s: err is %s", url, err.Error())
+			return
+		}
+		defer resp.Body.Close()
+		file, err := os.Create(fmt.Sprintf("%s/pic_%d.jpg", dirPath, i))
+		if err != nil {
+			log.Printf("fileCreate err: err is %s", err.Error())
+			return
+		}
+		defer file.Close()
+		io.Copy(file, resp.Body)
 	}
 }
